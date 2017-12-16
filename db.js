a0bf383e1ded738ee5470a0bc39933ceb10d2fbf
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-16 21:23:12 +0700

    xong

--- db.js
+++ db.js
@@ -10,6 +10,56 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
+// CRUD
+
+// const user = new User({ name: 'Ti', age: 10 });
+// user.save()
+// .then(u => console.log(u))
+// .catch(err => console.log(err));
+
+// User.insertMany([
+//     { name: 'Tun 2', age: 11 },
+//     { name: 'Khoa 3', age: 30 }
+// ])
+// .then(arr => console.log(arr))
+// .catch(err => console.log(err))
+
+// User.find({ age: 10 }, { name: 1, _id: 0 })
+// .then(users => console.log(users))
+// .catch(err => console.log(err));
+
+// User.findById('5a352329b041d508fa783030')
+// .then(user => console.log(user))
+// .catch(err => console.log(err));
+
+// User.findByIdAndUpdate('5a352329b041d508fa783030', { name: 'teo' })
+// .then(user => console.log(user))
+// .catch(err => console.log(err));
+
+// User.update({ age: 20 }, { age: 10 }, { multi: true })
+// .then(user => console.log(user))
+// .catch(err => console.log(err));
+
 User.find()
-.then(users => console.log(users))
+.then(user => console.log(user))
 .catch(err => console.log(err));
+
+// User.find
+// User.findById
+// User.findOne
+
+// User.update
+// User.findByIdAndUpdate
+// User.findOneAndUpdate
+
+// User.findByIdAndRemove('5a35280a01121d144707fd1a')
+// .then(user => console.log(user))
+// .catch(err => console.log(err));
+
+// User.findOneAndRemove({ name: 'TEO' })
+// .then(user => console.log(user))
+// .catch(err => console.log(err));
+
+// User.remove({ age: 30 })
+// .then(user => console.log(user))
+// .catch(err => console.log(err));
